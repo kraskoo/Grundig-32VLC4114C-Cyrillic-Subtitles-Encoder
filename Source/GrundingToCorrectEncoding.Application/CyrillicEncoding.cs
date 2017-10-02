cc98b2c92e3a18546c7ef04a1ded207835faeb0b
Krasimir Stefanov <kraskoo87@gmail.com>
2017-10-02 09:29:54 +0300

    Initial commit

---
+++ Source/GrundingToCorrectEncoding.Application/CyrillicEncoding.cs
@@ -0,0 +1,48 @@
+﻿namespace GrundingToCorrectEncoding.Application
+{
+    using System.Text;
+
+    public class CyrillicEncoding : Encoding
+    {
+        private readonly Encoding currentEncoding;
+
+        public CyrillicEncoding()
+        {
+            this.currentEncoding = Encoding.Default;
+        }
+
+        public override int GetByteCount(char[] chars, int index, int count)
+        {
+            return this.currentEncoding.GetByteCount(chars, index, count);
+        }
+
+        public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
+        {
+            return this.currentEncoding.GetBytes(chars, charIndex, charCount, bytes, byteIndex);
+        }
+
+        public override int GetCharCount(byte[] bytes, int index, int count)
+        {
+            return this.currentEncoding.GetCharCount(bytes, index, count);
+        }
+
+        public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
+        {
+            return this.currentEncoding.GetChars(bytes, byteIndex, byteCount, chars, charIndex);
+        }
+
+        public override int GetMaxByteCount(int charCount)
+        {
+            return this.currentEncoding.GetMaxByteCount(charCount);
+        }
+
+        public override int GetMaxCharCount(int byteCount)
+        {
+            return this.currentEncoding.GetMaxCharCount(byteCount);
+        }
+
+        public override string HeaderName => Encoding.UTF8.HeaderName;
+
+        public override string BodyName => Encoding.Default.BodyName;
+    }
+}
---
+++ Source/GrundingToCorrectEncoding.Application/EntryPoint.cs
@@ -0,0 +1,93 @@
+﻿namespace GrundingToCorrectEncoding.Application
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public static class EntryPoint
+    {
+        private static char[] cyrillicSymbols;
+
+        public static void Main()
+        {
+            Initialize();
+            Save(ConvertToEncodedTable(ReadFile(), out string newFileName), newFileName);
+        }
+
+        private static string ConvertToEncodedTable(string fileText, out string newFileName)
+        {
+            var copied = new char[cyrillicSymbols.Length];
+            Array.Copy(cyrillicSymbols, copied, copied.Length);
+            copied[48 + 0] = '\u00C0'; // А
+            copied[48 + 1] = (char)1118; // Б
+            copied[48 + 2] = (char)1030; // В
+            copied[48 + 3] = (char)1110; // Г
+            copied[48 + 4] = 'D'; // Д
+            copied[48 + 5] = 'E'; // Е
+            copied[48 + 8] = (char)1105; // И
+            copied[48 + 10] = (char)1108; // K
+            copied[48 + 12] = (char)1112; // М
+            copied[48 + 13] = (char)1029; // Н
+            copied[48 + 14] = (char)1109; // О
+            copied[48 + 15] = (char)1111; // П
+            for (int i = 0; i < 16; i++)
+            {
+                ShiftToLeft(copied);
+            }
+
+            var shiftedSymbols = cyrillicSymbols.Zip(
+                                    copied,
+                                    (o, c) => new KeyValuePair<char, char>(o, c))
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            shiftedSymbols['Ж'] = shiftedSymbols['ж'];
+            shiftedSymbols['З'] = shiftedSymbols['з'];
+            shiftedSymbols['Л'] = shiftedSymbols['л'];
+            shiftedSymbols['Й'] = shiftedSymbols['й'];
+            var output = new StringBuilder();
+            foreach (var @char in fileText)
+            {
+                output.Append(
+                    shiftedSymbols.ContainsKey(@char) ?
+                        shiftedSymbols[@char] :
+                        @char);
+            }
+
+            Console.Write("Enter path for encoded file: ");
+            newFileName = Console.ReadLine();
+            return output.ToString();
+        }
+
+        private static void Save(string fileText, string newFileName)
+        {
+            File.WriteAllText(newFileName, fileText, new CyrillicEncoding());
+        }
+
+        private static string ReadFile()
+        {
+            Console.Write("Enter path to subtitle file: ");
+            var file = Console.ReadLine();
+            return File.ReadAllText(file, Encoding.UTF8);
+        }
+
+        private static void ShiftToLeft<T>(T[] array)
+        {
+            var last = array[array.Length - 1];
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[0] = last;
+        }
+
+        private static void Initialize()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("bg-BG");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("bg-BG");
+            cyrillicSymbols = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+        }
+    }
+}
